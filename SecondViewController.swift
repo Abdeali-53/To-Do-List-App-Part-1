71e7748b39201b5acd56086b283adce213d698aa
Abdeali-53 <amody1@my.centennialcollege.ca>
2020-11-14 14:34:55 -0500

    Initial Commit

---
+++ SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  File Name: To-do_List_Assignment_4
+//
+//  Created by Abdeali Mody on 2020-11-11.
+//  Student ID: 301085484
+//  App Description: A To-Do List Application to Create Task, Edit Task & Delete Task.
+//  Copyright © 2020 Abdeali Mody. All rights reserved.
+//  Version 1.0.0
+
+import UIKit
+
+class SecondViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  File Name: To-do_List_Assignment_4
+//
+//  Created by Abdeali Mody on 2020-11-11.
+//  Student ID: 301085484
+//  App Description: A To-Do List Application to Create Task, Edit Task & Delete Task.
+//  Copyright © 2020 Abdeali Mody. All rights reserved.
+//  Version 1.0.0
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
